8818e4a02a697e8d5ace97522a7387826deb0b4c
VonIsaac <119748258+VonIsaac@users.noreply.github.com>
2024-08-16 23:53:57 +0800

    finishing the design

--- components/gadget/gadget-item.js
+++ components/gadget/gadget-item.js
@@ -4,26 +4,28 @@ import Image from 'next/image';
 
 export default function GadgetItem({ title, slug, image, summary, creator }) {
   return (
-    <article className = " text-white m-5" >
-      <header className = "  ">
+    <article className = " text-white m-5 bg-stone-800 py-5 px-2 w-[600px] rounded-lg" >
+      <header className = " flex justify-center items-center flex-col">
         <div   >
           <Image 
               src={image} 
               alt={title}  
               width={500}
               height={500} 
-              className = " w-[100px]" 
+              className = " w-[150px]" 
             />
         </div>
         <div >
-          <h2>{title}</h2>
-          <p>by {creator}</p>
+          <h2 className = " text-3xl font-bold mt-3 text-gray-600">{title}</h2>
+          <p className = " text-xs font-semibold text-gray-500 mb-4">by {creator}</p>
         </div>
       </header>
       <div>
-        <p >{summary}</p>
-        <div>
-          <Link href={`/gadgets/${slug}`}>View Details</Link>
+        <p className = " text-center font-semibold mb-5 text-lg">{summary}</p>
+        <div className = " text-end mx-3 ">
+          <Link href={`/gadgets/${slug}`} className ="bg-slate-900 px-2 py-2 text-lg font-bold rounded-md">
+            View Details
+          </Link>
         </div>
       </div>
     </article>
