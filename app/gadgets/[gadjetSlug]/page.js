b2a9baa38b9151ed32b89854354ed07c129875c3
VonIsaac <119748258+VonIsaac@users.noreply.github.com>
2024-08-16 23:53:00 +0800

    fetching all details of item

--- app/gadgets/[gadjetSlug]/page.js
+++ app/gadgets/[gadjetSlug]/page.js
@@ -1,10 +1,34 @@
-
+import Image from "next/image"
+import { getGadget } from "@/lib/gadgets"
 const GadgetsSlug = ({params}) => {
+    const gadget = getGadget(params.gadjetslug)
+
+    if(!gadget){
+        throw new Error('An Error Occured')
+    }
+    gadget.opinion = gadget.opinion.replace(/\n/g, '<br />')
     return (
-        <div>
-            <h1 className = " text-white">HELLO slug</h1>
-            <p className = "text-white"> This is the params of  {params.gadjetslug}</p>
-        </div>
+        <>
+            <header className = " text-white">
+                <div>
+                    <Image src = {gadget.image } alt = {gadget.title}  width={500} height={500} />
+                </div>
+                <div>
+                    <h1>{gadget.title}</h1>
+                    <p>
+                       by <a href = {`mailto:${gadget.creator_email}`}>{gadget.creator}</a>
+                    </p>
+                    <p>{gadget.summary}</p>
+                </div>
+                <main className = " text-white">
+                    <p dangerouslySetInnerHTML= {{
+                        __html: gadget.opinion
+                    }}>
+
+                    </p>
+                </main>
+            </header>
+        </>
     )
 }
 
