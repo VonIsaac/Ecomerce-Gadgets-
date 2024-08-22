761017af6af0d3d369aa8caf015f89408b52e35b
VonIsaac <119748258+VonIsaac@users.noreply.github.com>
2024-08-22 20:16:08 +0800

    reder the image picker

--- app/gadgets/shareGadgets/page.js
+++ app/gadgets/shareGadgets/page.js
@@ -1,4 +1,4 @@
-
+import ImagePicker from "@/components/images/image-picker";
 export default function ShareGadgetPage() {
 
     const classes = "bg-gray-400 font-semibold rounded focus:outline-none focus:ring-2 focus:ring-neutral-600 px-2 py-1 focus:outline-neutral-ring-neutral-600 w-[25rem]"
@@ -41,7 +41,7 @@ export default function ShareGadgetPage() {
               className = " rounded-md bg-neutral-500 font-bold px-4 py-2 text-xl "
             ></textarea>
           </p>
-          IMAGE PICKER
+          <ImagePicker label= "Your Image" name= "image" />
           <p className = " flex items-end justify-end my-5">
             <button type="submit" className = " bg-neutral-700 px-3 py-1 rounded font-bold uppercase">
                 Share Gadgets
